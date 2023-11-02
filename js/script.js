e18e3f0b969a56005f1b22e7027b33bd95cddacd
Sebastian Siddi <ssiddi@gmail.com>
2023-11-02 17:12:41 -0300

    segunda pre entrega

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// Calculadora Anti Inflación
+// Menú de comidas
+//Laa Función del IVA queda de la entre anterior para utilizar luego si hicera falta
 
 // Calcula el interés compuesto de un importe dado según el interés anual y la cantidad de meses
 function interesCompuesto (sumaInicial, cantMeses, porcentaje){
@@ -9,17 +10,17 @@ function interesCompuesto (sumaInicial, cantMeses, porcentaje){
 }
 
 // Función de orden superior para calcular el valor de un producto sin IVA
-function calcularSinIVA(porcentajeIVA) {
+function calcularDescuento(porcentaje) {
   return function(importe) {
-    return (importe - (importe * porcentajeIVA / 100)).toFixed(2);
+    return (importe - (importe * porcentaje / 100)).toFixed(2);
   };
 }
 
-// Función para calcular sin IVA al 21%
-const sinIVA21 = calcularSinIVA(21);
+// Función para calcular descuento del 20%
+const descuento20 = calcularDescuento(20);
 
-// Función para calcular sin IVA al 10.5%
-const sinIVA10 = calcularSinIVA(10.5);
+// Función para calcular descuento del 10%
+const descuento10 = calcularDescuento(10);
 
 
 // Calcula el valor del IVA aplicado a un producto
@@ -27,37 +28,41 @@ function devIVA (importe){
   return ((importe - sinIVA21(importe)).toFixed(2))
 }
 
-// Calcula cuál es el tope de compra para aprovechar un descuento por porcentaje pero con tope de descuento
-function calcularPagoOptimo(desc, tope){
-  return(((100*tope)/desc).toFixed(2));
- }
- 
- // Calcula precio por kilo de un producto
- function precioPorKilo(precio, gramos){
-  return(((1000*precio)/gramos).toFixed(2));
- }
+function verProductos(lista){
+let listaString = []
+lista.forEach(element => {
+  listaString= listaString + (element.id +" - "+ element.comida + ": $" +element.precio +"\n")
+});
+return (listaString);
+}
 
+// Función para filtrar los alimentos catalogados como veganos
+function veganos(lista){
+  const veganosFiltrados = lista.filter((el) => el.vegano == true);
+  console.log(veganosFiltrados);
+  return verProductos(veganosFiltrados);
+}
 
+class Menu{
+  constructor(id, comida, precio, vegano){
+    this.id = id;
+    this.comida = comida;
+    this.precio = precio;
+    this.vegano = vegano;
+  }
 
- function verProductos(lista){
-  let listaString = []
-  lista.forEach(element => {
-    listaString= listaString + (element.id +" - "+ element.comida + ": $" +element.precio +"\n")
-  });
-  return (listaString);
- }
-
+}
 
 const listaProductos = [
-  {id: 1, comida: "Quesadillas de Pollo y Vegetales",precio: 2000},
-  {id: 2, comida: "Pastel de Calabaza, Espinaca y Pollo",precio: 2000},
-  {id: 3, comida: "Pastel de Batata Boniatto con Pollo a la Mostaza",precio: 2100},
-  {id: 4, comida: "Musaka Griega (Con Carne y Berenjenas Asadas)",precio: 2100},
-  {id: 5, comida: "Berenjenas Napolitanas",precio: 2200},
-  {id: 6, comida: "Nituke (Mil Hojas de Vegetales con Salsa Blanca y Parmesano)",precio: 2200},
-  {id: 7, comida: "Ratatoulille de Vegetales con Pollo",precio: 2300},
-  {id: 8, comida: "Omelette de Espinaca y Parmesano",precio: 2300},
-  {id: 9, comida: "Risotto de Pollo y Verdeo",precio: 2400},
+  new Menu (1,"Quesadillas de Pollo y Vegetales",2000,false),
+  new Menu (2,"Pastel de Calabaza, Espinaca y Pollo",2000, false),
+  new Menu (3,"Pastel de Batata Boniatto con Pollo a la Mostaza",2100,false),
+  new Menu (4,"Musaka Griega (Con Carne y Berenjenas Asadas)",2100,false),
+  new Menu (5,"Berenjenas Napolitanas",2200,true),
+  new Menu (6,"Nituke (Mil Hojas de Vegetales con Salsa Blanca y Parmesano)",2200,true),
+  new Menu (7,"Ratatoulille de Vegetales con Pollo",2300,false),
+  new Menu (8,"Omelette de Espinaca y Parmesano",2300,true),
+  new Menu (9,"Risotto de Pollo y Verdeo",2400,false)
 ]
 
 
@@ -65,8 +70,7 @@ const listaProductos = [
 let option = "";
 let optionSub1 = 1;
 while (option !== "0"){
- // option = prompt("1- Interés compuesto. 2- Restar IVA 21%. 3- Restar IVA 10.5%. 4-Calcular devolución de IVA. 5-Compra óptima. 6-Precio por Kilo. 0-SALIR")
-  option = prompt("1- Ver Menú\n0-SALIR")
+  option = prompt("1- Ver Menú \n 2-Ver comida Vegana \n 0-SALIR")
   if (option == "0") {
     break;
   }
@@ -75,17 +79,26 @@ while (option !== "0"){
       case "0": 
         break;
       case "1":
+
         while (optionSub1 !== 0){
           optionSub1 = prompt(verProductos(listaProductos)+"\n 0 para Volver");
           if (optionSub1 == 0){
             break;
           }
           else if (optionSub1 < 10){
-            alert("opción elegida "+listaProductos[optionSub1-1].comida)
+            if (listaProductos[optionSub1-1].vegano){
+              alert("A los prodctos veganos les corresponde un desuento del 20%, el precio final del producto es $"+descuento20(listaProductos[optionSub1-1].precio));
+            }
+            else{
+              alert("A los prodctos No veganos les corresponde un descuento del 10%, el precio final del producto es $"+descuento10(listaProductos[optionSub1-1].precio));
+            }
           }
           else {alert("opcion incorrecta")}
         }
-          
+
+        break;
+      case "2":
+        alert("Lista de productos veganos: \n"+veganos(listaProductos))
         break;
       default:
         alert("opción incorrecta");
